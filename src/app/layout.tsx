b86a2817badd2f0135ecb193f21a4ec85c7de7fb
Nostalgic-math-student <josue.rojas@ciencias.unam.mx>
2024-08-10 15:24:20 -0600

    basic frontend setup on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
-
-import { GeistSans } from "geist/font/sans";
+import { Roboto } from "next/font/google";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -9,12 +8,19 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+// Importing the Roboto font with different weights
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"], // Add weights as needed
+  variable: "--font-roboto",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={roboto.variable}>
+      <body className="font-sans">{children}</body>
     </html>
   );
 }
---
+++ src/components/Animation.tsx
@@ -0,0 +1,40 @@
+"use client";
+import "./styles.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const color = "#00FFFF";
+
+const RotatingTorus = () => {
+  const ref = useRef<THREE.Mesh>(null);
+
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.x += 0.006 ;
+      ref.current.rotation.y += 0.006;
+    }
+  });
+
+  return (
+    <mesh ref={ref}>
+      <torusKnotGeometry args={[1.3, 0.55, 120, 20]} />
+      <meshBasicMaterial wireframe color={color} />
+    </mesh>
+  );
+};
+
+function Scene() {
+  return (
+    <RotatingTorus />
+  );
+}
+
+export default function Animation() {
+  return (
+    <div className="fixed inset-0 w-full h-full z-0">
+      <Canvas gl={{ antialias: true }}>
+        <Scene />
+      </Canvas>
+    </div>
+  );
+}
